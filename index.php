6d37ba7ea9ee023ac041c6211d52a915f531c13b
HarshRookie <harshrathi71@gmail.com>
2018-03-20 21:28:35 +0530

    Added index code

--- index.php
+++ index.php
@@ -1,1 +1,29 @@
-<?php echo "Hello World"; ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.40/css/uikit.min.css" />
+
+</head>
+<body>
+
+	<form>
+
+    <div class="uk-margin">
+        <input class="uk-input uk-form-width-medium uk-form-large" type="text" placeholder="Large">
+    </div>
+
+    <div class="uk-margin">
+        <input class="uk-input uk-form-width-medium" type="text" placeholder="Default">
+    </div>
+
+    <div class="uk-margin">
+        <input class="uk-input uk-form-width-medium uk-form-small" type="text" placeholder="Small">
+    </div>
+
+</form>
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.40/js/uikit.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.40/js/uikit-icons.min.js"></script>
+
+</body>
+</html>
