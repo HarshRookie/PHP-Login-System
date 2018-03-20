2582bda0600bd2e695903c1e80749be7f3374d58
HarshRookie <harshrathi71@gmail.com>
2018-03-20 22:56:58 +0530

    Added index code

--- index.php
+++ index.php
@@ -6,21 +6,37 @@
 </head>
 <body>
 
+<div class="uk-section uk-container">
+    <div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" ukgrid="">
 	<form>
 
     <div class="uk-margin">
-        <input class="uk-input uk-form-width-medium uk-form-large" type="text" placeholder="Large">
+        <label class="uk-form-label">Email</label>
+        <div class="uk-form-controls">
+        <input class="uk-input uk-form-width-medium uk-form-large" type="text" required ='required' placeholder="someemail@example.com">
     </div>
+</div>
 
     <div class="uk-margin">
-        <input class="uk-input uk-form-width-medium" type="text" placeholder="Default">
+        <label class="uk-form-label">Password</label>
+        <div class="uk-form-controls">
+        <input class="uk-input uk-form-width-medium" type="password" placeholder="Password">
     </div>
+</div>
 
     <div class="uk-margin">
-        <input class="uk-input uk-form-width-medium uk-form-small" type="text" placeholder="Small">
+         <label class="uk-form-label">Select</label>
+         <div class="uk-form-controls">
+        <select class="uk-select">
+            <option>Option 1</option>
+            <option>Option 2</option>
+        </select>
     </div>
+</div>
 
 </form>
+</div>
+</div>
 
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.40/js/uikit.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.40/js/uikit-icons.min.js"></script>
